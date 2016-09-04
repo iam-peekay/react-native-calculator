ea7c45fa9ad051a494439166af4048e120719f6a
Preethi Kasireddy <preethi@coinbase.com>
2016-09-04 12:02:47 -0700

    Create common src file for both ios and android

--- index.android.js
+++ index.android.js
@@ -1,15 +1,1 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Text,
-} from 'react-native';
-
-class ReactCalculator extends Component {
-  render() {
-    return (
-      <Text> Hello, World! </Text>
-    )
-  }
-}
-
-AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
+import ReactCalculator from './src/ReactCalculator';
